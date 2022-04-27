6148724c6e05f301707a0021fb63b8ecc5fff889
alnkravchenko <alenakravchenko1@gmail.com>
2022-04-27 15:39:22 +0300

    add folder for DOM and task 1

---
+++ dom/project/js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const ads = document.querySelectorAll(".promo__adv img");
+ads.forEach(item => { item.remove(); });
+
+
+const poster = document.querySelector(".promo__bg");
+const genre = poster.querySelector(".promo__genre");
+genre.textContent = "drama";
+
+
+poster.style.backgroundImage = "url('./img/bg.jpg')";
+
+
+const movieList = document.querySelector(".promo__interactive-list");
+// Delete old
+const movies = movieList.querySelectorAll(".promo__interactive-item");
+movies.forEach(item => { item.remove(); });
+
+const sortedMovies = movieDB.movies.sort();
+
+sortedMovies.forEach((movie, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${movie}
+      <div class="delete"></div>
+    </li>
+  `;
+});
