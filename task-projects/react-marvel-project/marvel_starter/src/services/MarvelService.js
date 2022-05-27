3159ce2bbfeebd3503c397aec97cf5227b48dae6
alnkravchenko <alenakravchenko1@gmail.com>
2022-05-28 01:29:38 +0300

    add Random Character implementation

--- task-projects/react-marvel-project/marvel_starter/src/services/MarvelService.js
+++ task-projects/react-marvel-project/marvel_starter/src/services/MarvelService.js
@@ -13,13 +13,31 @@ export default class MarvelService {
         return await res.json();
     };
 
-    getAllCharacters = () => {
+    getAllCharacters = async () => {
         const url = this._baseURL + `characters?limit=9&offset=210&apikey=${publicKey}`;
-        return this.getResource(url);
+        const res = await this.getResource(url);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacterById = id => {
+    getCharacterById = async id => {
         const url = this._baseURL + `characters/${id}?apikey=${publicKey}`;
-        return this.getResource(url);
+        const res = await this.getResource(url);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = charData => {
+        const thumbnail = Object.values(charData.thumbnail).join(".");
+        if (charData.description.length == 0) {
+            charData.description = "Character description not found"
+        } else if (charData.description.length > 230) {
+            charData.description = charData.description.slice(0, 230) + "...";
+        }
+        return {
+            name: charData.name,
+            description: charData.description,
+            thumbnail: thumbnail,
+            homepage: charData.urls[0].url,
+            wiki: charData.urls[1].url
+        };
     }
 }
