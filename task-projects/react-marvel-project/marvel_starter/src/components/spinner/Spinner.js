6e1e8efaf0fcb790cf41217a02fa0be1908e571d
alnkravchenko <alenakravchenko1@gmail.com>
2022-05-28 22:55:19 +0300

    change spinner colors

--- task-projects/react-marvel-project/marvel_starter/src/components/spinner/Spinner.js
+++ task-projects/react-marvel-project/marvel_starter/src/components/spinner/Spinner.js
@@ -2,7 +2,7 @@ const Spinner = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" style={{ margin: '0 auto', background: 'none', display: 'block' }} width="300px" height="300px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
             <g transform="rotate(0 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#ec1c24">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#9F0013">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-1.3043478260869565s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(36 50 50)">
@@ -10,11 +10,11 @@ const Spinner = () => {
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-1.1594202898550725s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(72 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#000000">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#0b0b31">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-1.0144927536231882s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(108 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#ec1c24">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#9F0013">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.8695652173913043s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(144 50 50)">
@@ -22,11 +22,11 @@ const Spinner = () => {
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.7246376811594203s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(180 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#000000">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#0b0b31">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.5797101449275363s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(216 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#ec1c24">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#9F0013">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.43478260869565216s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(252 50 50)">
@@ -34,11 +34,11 @@ const Spinner = () => {
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.2898550724637681s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(288 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#000000">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#0b0b31">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="-0.14492753623188406s" repeatCount="indefinite"></animate>
                 </rect>
             </g><g transform="rotate(324 50 50)">
-                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#ec1c24">
+                <rect x="46.5" y="26" rx="3.5" ry="4" width="7" height="8" fill="#9F0013">
                     <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.4492753623188404s" begin="0s" repeatCount="indefinite"></animate>
                 </rect>
             </g>
